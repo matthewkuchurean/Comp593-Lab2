9b935c7b631c46d710a6c8eefb3d89287606e0f2
matthewkuchurean <matthew.kuchurean@flemingcollege.ca>
2023-02-01 12:37:39 -0500

    Update Lab2-Complex_Data_Structures.py

--- Lab2-Complex_Data_Structures.py
+++ Lab2-Complex_Data_Structures.py
@@ -1,90 +1,63 @@
-def main() :
+# Create a Complex Data Structure 
+def main():
     about_me = {
-        "Student_Name": "Matt Kuchurean",
+        "Student_Name": "Matthew Kuchurean",
         "Student_ID": 10210143,
-        "pizza_toppings": ["Cheese", "Green Pepper", "Pepperoni"],
+        "pizza_toppings": ["cheese", "green pepper", "Pepperoni"],
         "Movies": [
             {
                 "Title": "Harry Potter and the Chamber of Secret",
-                "Genre": "Adventure"
+                "Genre": ["Sci-Fi", "Adventure"]
             },
             {
-                "Title": "The Might Ducks",
-                "Genre": ["Drama", "Comedy"]
+                "Title": "Mighty Ducks",
+                "Genre": ["Drama", "Action"]
             }
         ]
     }
     return about_me
-
-    # Created a Function to define details about me and adding a movie to the data structure......
-def Movies(New_Movie, about_me):
+# Add of New Movie
+def Movies_new(details, New_Movie, about_me):
     New_Movie = {"Title": "Lethal Weapon", "Genre": ["Action"]}
     about_me["Movies"].append(New_Movie)
 
-   
-    #Step 4 - Function that prints student name and IDdef printing_sentence(details) : 
-    # TODO: Step 4 - Function that prints student name and ID
-    details =0
-    Function = f"My name is" + {details['Student_Name']} + "But you can call me Lord" + {details["Student_Name"].removesuffix('Gogia')} + '.'\
-    "And My Student ID is" + {details["Student_ID", "end = '\n\n'"]}  
-    print(Function , "end = '\n\n'", "end = '\n\n'")
-
-
-# Step 5 - Function that adds pizza toppings to data structure
-     #adding new topping 
-    details["Topping"].extend(New_Topping)
-
-    # Adding a new topping 
-    New_Topping = ['pizza seasoning']
-    about_me["pizza_toppings"].append(New_Topping)
-
-
-    #lowercasing the letters
-    for i,a in enumerate(details['topping']) :
-        details['topping'][i] = a.lower()
-    
-    #sorting the toppings in alphabetical order
-    details['topping'].sort()
-
-    
-#  Step 6 - Function that prints bullet list of pizza toppings
-#printing the toppings with bullets
-def bullet_new_toppings(student_details) :
-    print("My favourite toppings are:")
-
-    for p in student_details['topping']:
-        print(f"- {p}")
+# Add of Function of Student Name and ID 
+def aboutme_sentence(details):
+    name = details["Student_Name"]
+    id = details["Student_ID"]
+    print(f"My name is {name}. You can call me Lord {name.rsplit(' ', 1)[0]}."
+          f" My student ID is {id}")
+# Add Function of New Pizza Topping 
+def add__newpizza_topping(details, new_topping):
+    details["pizza_toppings"].append(new_topping)
+    for i, topping in enumerate(details["pizza_toppings"]):
+        details["pizza_toppings"][i] = topping.lower()
+    details["pizza_toppings"].sort()
+# Favourite Pizza Topping
+def print_Fav_pizza_toppings(details):
+    print("My favorite pizza toppings are:")
+    for topping in details["pizza_toppings"]:
+        print(f"- {topping}")
     print()
-
-
-#Step 7 - Function that prints comma-separated list of movie genres
-#printing genres
-def comma_goto_genre(student_details) :
-    print(f"My goto genres are", end='')
-    for i,g in enumerate(student_details['movies']) :
-        print(g['genre'], end='')
-        if i < len(student_details['movies'])-1:
-            print(', ', end='')
-    print('.', end='\n\n')
-
-#Step 8 - Function that prints comma-separated list of movie titles
-#printing movies
-def comma_list_genr(student_details) :
-    print(f"My favourite movies are", end='')
-    for i,g in enumerate(student_details['movies']) :
-        print(g['title'], end='')
-        if i < len(student_details['movies'])-1:
-            print(', ', end='')
-    print('.', end='\n\n')
-
-#calling the main function
-main()
-Movies(main)
-bullet_new_toppings(main)
-comma_goto_genre(main)
-comma_list_genr(main)
-
-
-
-
-
+# Function of Go to Movies and Genre 
+def print_watch_genres(details):
+    print("I like to watch ", end="")
+    genres = []
+    for movie in details["Movies"]:
+        genres.extend(movie["Genre"])
+    print(", ".join(genres), end=".")
+    print()
+# Fav Movie Titles 
+def print_Fav_movie_titles(details):
+    print("Some of my favorite movie titles are: ", end="")
+    titles = [movie["Title"] for movie in details["Movies"]]
+    print(", ".join(titles), end=".")
+    print()
+# Main Output
+details = main()
+Movies_new(details, "Pepper", details)
+aboutme_sentence(details)
+add__newpizza_topping(details, "Pizza sauce")
+print_Fav_pizza_toppings(details)
+print_watch_genres(details)
+print_Fav_movie_titles(details)
